8c314045bdb9266999fb17655bf0eec2155fd261
Djuzik <lolkab25@gmail.com>
2020-11-25 14:26:23 +0300

    all new

--- three/js/javaScript.js
+++ three/js/javaScript.js
@@ -1,55 +1,53 @@
+var scene = new THREE.Scene();
 
-      let scene, camera, renderer;
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.z = 200;
 
-      function init() {
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-        scene = new THREE.Scene();
-        scene.background = new THREE.Color(0xdddddd);
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+controls.enableZoom = true;
 
-        camera = new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight,1,5000);
-        camera.rotation.y = 45/180*Math.PI;
-        camera.position.x = 800;
-        camera.position.y = 100;
-        camera.position.z = 1000;
+var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
+keyLight.position.set(-100, 0, 100);
 
-        //controls = new THREE.OrbitControls(camera);
-        //controls.addEventListener('change', renderer);
+var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
+fillLight.position.set(100, 0, 100);
 
-        //hlight = new THREE.AmbientLight (0x404040,100);
-        //scene.add(hlight);
+var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+backLight.position.set(100, 0, -100).normalize();
 
-        directionalLight = new THREE.DirectionalLight(0xffffff,100);
-        directionalLight.position.set(0,1,0);
-        directionalLight.castShadow = true;
-        scene.add(directionalLight);
-        light = new THREE.PointLight(0xc4c4c4,10);
-        light.position.set(0,300,500);
-        scene.add(light);
-        light2 = new THREE.PointLight(0xc4c4c4,10);
-        light2.position.set(500,100,0);
-        scene.add(light2);
-        light3 = new THREE.PointLight(0xc4c4c4,10);
-        light3.position.set(0,100,-500);
-        scene.add(light3);
-        light4 = new THREE.PointLight(0xc4c4c4,10);
-        light4.position.set(-500,300,500);
-        scene.add(light4);
-        
+scene.add(keyLight);
+scene.add(fillLight);
+scene.add(backLight);
 
-        renderer = new THREE.WebGLRenderer({antialias:true});
-        renderer.setSize(window.innerWidth,window.innerHeight);
-        document.body.appendChild(renderer.domElement);
+var mtlLoader = new THREE.MTLLoader();
+mtlLoader.setTexturePath('/examples/3d-obj-loader/assets/');
+mtlLoader.setPath('/examples/3d-obj-loader/assets/');
+mtlLoader.load('r2-d2.mtl', function (materials) {
 
-        let loader = new THREE.GLTFLoader();
-        loader.load('scene.gltf', function(gltf){
-          car = gltf.scene.children[0];
-          car.scale.set(0.5,0.5,0.5);
-          scene.add(gltf.scene);
-          animate();
-        });
-      }
-      function animate() {
-        renderer.render(scene,camera);
-        requestAnimationFrame(animate);
-      }
-      init();
+    materials.preload();
+
+    var objLoader = new THREE.OBJLoader();
+    objLoader.setMaterials(materials);
+    objLoader.setPath('/examples/3d-obj-loader/assets/');
+    objLoader.load('r2-d2.obj', function (object) {
+
+        scene.add(object);
+        object.position.y -= 60;
+
+    });
+
+});
+
+var animate = function () {
+  requestAnimationFrame( animate );
+  controls.update();
+  renderer.render(scene, camera);
+};
+
+animate();
